222bbd5ee54c9e64657f9c1777dbc79ac163cb2d
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-03-02 05:50:11 -0400

    feat: adicionado email na hora do cadastro

--- app/controllers/cadastrar_controller.py
+++ app/controllers/cadastrar_controller.py
@@ -10,10 +10,11 @@ def cadastrar_controller(app):
         if request.method == 'GET':
             return render_template('cadastrar.html')
         elif request.method == 'POST':
-            nome = request.form['nomeForm']
-            senha = request.form['senhaForm']
+            nome = request.form['nome']
+            senha = request.form['senha']
+            email = request.form['email']
 
-            novo_usuario = Usuarios(nome=nome, senha=senha)
+            novo_usuario = Usuarios(nome=nome, senha=senha, email=email)
             db.session.add(novo_usuario)
             db.session.commit()
 
